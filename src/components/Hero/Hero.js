5f101e283fe6b4d977457a1297931d00d7112720
Agbley <agbleyjill@gmail.com>
2023-12-14 21:46:02 +0000

    first commit

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+import {
+  Section,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import Button from "../../styles/GlobalComponents/Button";
+import { LeftSection } from "./HeroStyles";
+
+const Hero = () => (
+  <Section>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome To <br />
+        Jill Agbley's Personal Developer Portfolio
+      </SectionTitle>
+      <SectionText>
+        User-Focused Innovation: My Front-End Development, your digital
+        architect. I am Proficient in HTML, CSS, JavaScript, React.js,
+        typescript and Next.js. I forge user interfaces that inspire. Skilled in
+        frameworks and responsive design, I ensure seamless interactions.
+        Collaborating with designers, I breathe life into creative visions. My
+        expertise bridge design and functionality, shaping captivating web
+        experiences.{" "}
+      </SectionText>
+      <Button onClick={() => (window.location = "http://google.com")}>
+        Learn More
+      </Button>
+    </LeftSection>
+  </Section>
+);
+
+export default Hero;
